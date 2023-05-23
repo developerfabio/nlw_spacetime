a225e05e0ed9a36137d41c0015ede7a8e51d95b4
Fabio <fabiofullstackdeveloper@gmail.com>
2023-05-22 23:30:41 -0300

    NLW Spacetime

---
+++ README.md
@@ -0,0 +1,50 @@
+<img src="./mobile/assets/app-web.png" />
+<div align="center">
+  <img align="center" src="./mobile/assets/app-mobile.png" />
+</div>
+<br/><br/><br/>
+
+# NLW Spacetime
+<br/>
+
+Projeto desenvolvido durante o evento NLW Spacetime realizado pela <a href="https://www.rocketseat.com.br/ ">Rocketseat</a>.
+<br/><br/>
+
+## 🛠️ Tecnologias
+
+Esse projeto foi desenvolvido com as seguintes tecnologias:
+
+* [Next.js](https://nextjs.org/) - Usado no app web
+* [Node.js](https://nodejs.org/en) - Usado no backend
+* [Expo](https://expo.dev/) - Usado no app mobile
+
+<br/>
+
+## 🚀 Começando
+
+Essas instruções, permitirão que você obtenha uma cópia do projeto em operação na sua máquina local. Para fins de desenvolvimento e teste.
+<br/><br/><br/>
+
+### 📋 Pré-requisitos
+
+Ter o Noje.js instalado em seu computador, para poder instalar as dependências do projeto.
+<br/><br/><br/>
+
+### 🔧 Instalação
+
+Abra o terminal na pasta de cada projeto (web, backend e mobile), rode o comando
+
+```
+npm i
+```
+Após executar o comando, aguarde instalar todas as dependências.
+<br/><br/><br/>
+
+### ⚙️ Configuração
+
+Criar o arquivo .env na raiz do projeto backend, para configurar as variáveis ambiente *GITHUB_CLIENT_ID* e *GITHUB_CLIENT_SECRET*. Tanto do app web, quanto do app mobile.<br/>
+Criar o arquivo .env.local do projeto web, para configurar a variável de ambiente *NEXT_PUBLIC_GITHUB_CLIENT_ID*.
+<br/><br/><br/>
+
+---
+⌨️ com ❤️ por [Developer Fabio](https://gist.github.com/developerfabio) 😊
---
+++ mobile/app/index.tsx
@@ -0,0 +1,106 @@
+import { useEffect } from 'react'
+import { useRouter } from 'expo-router'
+import { Text, TouchableOpacity, View } from 'react-native'
+import { makeRedirectUri, useAuthRequest } from 'expo-auth-session'
+import * as SecureStore from 'expo-secure-store'
+
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from '@expo-google-fonts/roboto'
+
+import { BaiJamjuree_700Bold } from '@expo-google-fonts/bai-jamjuree'
+
+import NLWLogo from '../src/assets/nlw-spacetime-logo.svg'
+import { api } from '../src/lib/api'
+
+const discovery = {
+  authorizationEndpoint: 'https://github.com/login/oauth/authorize',
+  tokenEndpoint: 'https://github.com/login/oauth/access_token',
+  revocationEndpoint:
+    'https://github.com/settings/connections/applications/90ea9ef6756a0d31aee2',
+}
+
+export default function App() {
+  const router = useRouter()
+
+  const [hasLoadedFonts] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+    BaiJamjuree_700Bold,
+  })
+
+  const [, response, signInWithGithub] = useAuthRequest(
+    {
+      clientId: '90ea9ef6756a0d31aee2',
+      scopes: ['identity'],
+      redirectUri: makeRedirectUri({
+        scheme: 'nlwspacetime',
+      }),
+    },
+    discovery,
+  )
+
+  async function handleGithubOAuthCode(code: string) {
+    const response = await api.post('/register', {
+      code,
+    })
+
+    const { token } = response.data
+
+    await SecureStore.setItemAsync('token', token)
+
+    router.push('/memories')
+  }
+
+  useEffect(() => {
+    // console.log(
+    //   makeRedirectUri({
+    //     scheme: 'nlwspacetime',
+    //   }),
+    // )
+
+    if (response?.type === 'success') {
+      const { code } = response.params
+
+      handleGithubOAuthCode(code)
+    }
+  }, [response])
+
+  if (!hasLoadedFonts) {
+    return null
+  }
+
+  return (
+    <View className="flex-1 items-center px-10 py-10">
+      <View className="flex-1 items-center justify-center gap-6">
+        <NLWLogo />
+
+        <View className="space-y-2">
+          <Text className="text-center font-title text-2xl leading-tight text-gray-50">
+            Sua cápsula do tempo
+          </Text>
+          <Text className="text-center font-body text-base leading-relaxed text-gray-100">
+            Colecione momentos marcantes da sua jornada e compartilhe (se
+            quiser) com o mundo!
+          </Text>
+        </View>
+
+        <TouchableOpacity
+          activeOpacity={0.7}
+          className="rounded-full bg-green-500 px-5 py-2"
+          onPress={() => signInWithGithub()}
+        >
+          <Text className="font-alt text-sm uppercase text-black">
+            Começar a cadastrar
+          </Text>
+        </TouchableOpacity>
+      </View>
+
+      <Text className="text-center font-body text-sm leading-relaxed text-gray-200">
+        Feito com 💜 no NLW da Rocketseat
+      </Text>
+    </View>
+  )
+}
